7905fb9baaf860a97893adf72b75ce0727a890ff
theripper1116 <milanagarwallaj@yahoo.in>
2024-05-26 17:28:16 +0530

    May 26 commit 1

---
+++ Modules/person.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  work: {
+    type: String,
+    enum: ["chef", "waiter", "manager"],
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+  },
+  salary: {
+    type: String,
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+module.exports = Person;
--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const db = require("./db");
+const Person = require("./Modules/person");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  console.log("server live");
 });
 
-app.listen(3000);
+app.post("/storeData", async (req, res) => {
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
+    const savedPerson = await newPerson.save();
+    res.status(200).json(savedPerson);
+  } catch (err) {
+    res.status(500).json("Error Occured");
+  }
+});
+
+app.listen(1234, () => {
+  console.log("Connection is live");
+});
